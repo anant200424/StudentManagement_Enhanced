cdea370c759429846decf463e9023b2b6946001b
anant200424 <anantkumar22022@gmail.com>
2026-03-31 10:52:53 +0530

    fixed webapp path

---
+++ src/main/java/com/student/util/Main.java
@@ -0,0 +1,39 @@
+package com.student.util;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        String port = System.getenv("PORT");
+        if (port == null) port = "8080";
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(Integer.parseInt(port));
+        tomcat.getConnector();
+
+        File webappDir = new File("src/main/webapp");
+        if (!webappDir.exists()) {
+            webappDir = new File("webapp");
+        }
+
+        Context ctx = tomcat.addWebapp("", webappDir.getAbsolutePath());
+
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(
+            resources,
+            "/WEB-INF/classes",
+            new File("target/classes").getAbsolutePath(),
+            "/"
+        ));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
